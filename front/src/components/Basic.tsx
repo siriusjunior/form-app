f7b0b52d9d277f9fbf9740cc8d5dca1f53109ae2
siriusjunior <topdownreading@gmail.com>
2022-07-06 06:38:36 +0900

    [Add]RadioボタンUI,redux接続

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { TextField } from "@material-ui/core";
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@material-ui/core";
 import useStyles from "./styles";
 import { RootState } from "../domain/entity/rootState";
 import { Profile } from "../domain/entity/profile";
+import { Gender } from "../domain/entity/gender";
 import { PROFILE } from "../domain/services/profile";
 import profileActions from "../store/profile/actions";
 
@@ -23,7 +31,7 @@ const Basic = () => {
         className={classes.formField}
         value={profile.name}
         label={PROFILE.NAME}
-        // 'name' exists in type 'Partial<Profile>'
+        // 'name' exists in type 'Partial<Profile>',cfL23
         onChange={(e) => handleChange({ name: e.target.value })}
       />
       <TextField
@@ -33,9 +41,28 @@ const Basic = () => {
         rows={5}
         label={PROFILE.DESCRIPTION}
         value={profile.description}
-        // 'description' exists in type 'Partial<Profile>'
+        // 'description' exists in type 'Partial<Profile>',cfL23
         onChange={(e) => handleChange({ description: e.target.value })}
       />
+      <FormControl className={classes.formField}>
+        <FormLabel>{PROFILE.GENDER}</FormLabel>
+        <RadioGroup
+          value={profile.gender}
+          // 'gender' exists in type 'Partial<Profile>',cfL23
+          onChange={(e) => handleChange({ gender: e.target.value as Gender })}
+        >
+          <FormControlLabel
+            value="male"
+            label="男性"
+            control={<Radio color="primary" />}
+          />
+          <FormControlLabel
+            value="female"
+            label="女性"
+            control={<Radio color="primary" />}
+          />
+        </RadioGroup>
+      </FormControl>
     </>
   );
 };
